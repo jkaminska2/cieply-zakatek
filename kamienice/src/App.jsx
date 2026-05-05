447e01654a996a7908c65ea7cae644e104e30f7a
igaglow <93049164+igaglow@users.noreply.github.com>
2026-05-05 16:42:38 +0200

    Dodanie mojego komponentu :3

--- kamienice/src/App.jsx
+++ kamienice/src/App.jsx
@@ -3,12 +3,12 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
+import Form from "./components/Form";
 
 export default function App() {
   return (
     <div class="app">
       <h1>Trzymaj Się Ciepło!!!</h1>
-      
     </div>
-  )
+  );
 }
