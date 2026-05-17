956bd309a075c5a225676b5a18c2f8b1c0dddb24
jkaminska2 <joanna.kam55@gmail.com>
2026-05-17 22:43:37 +0200

    Dodanie hamburgera

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,32 @@
+import { useState } from "react";
 import "../styles/NavBar.css";
 
 export default function Navbar() {
-  return (
-    <nav className="navbar">
-      <h2 className="logo">Ciepły zakątek</h2>
+  const [open, setOpen] = useState(false);
 
-      <ul className="nav-links">
-        <li><a href="#">Strona główna</a></li>
-        <li><a href="#">Forum</a></li>
-        <li><a href="#">Fundacje</a></li>
-        <li><a href="#">Dofinansowania</a></li>
-      </ul>
-    </nav>
+  return (
+    <>
+      <nav className="navbar">
+        <h2 className="logo">Ciepły zakątek</h2>
+
+        <div
+          className={`hamburger ${open ? "open" : ""}`}
+          onClick={() => setOpen(!open)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <ul className={`nav-links ${open ? "active" : ""}`}>
+          <li><a href="#">Strona główna</a></li>
+          <li><a href="#">Forum</a></li>
+          <li><a href="#">Fundacje</a></li>
+          <li><a href="#">Dofinansowania</a></li>
+        </ul>
+      </nav>
+
+      {open && <div className="overlay" onClick={() => setOpen(false)}></div>}
+    </>
   );
 }
